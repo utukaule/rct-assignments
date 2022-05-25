71d1e62bf9a91becc55d7a6f7b0d1b43f01ab5d6
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-05-25 11:16:33 +0530

    Css added to this

--- TodoList/todo/src/components/TodoItem.jsx
+++ TodoList/todo/src/components/TodoItem.jsx
@@ -5,9 +5,19 @@ export const TodoItem = ({ todo, onDelete }) => {
     const [isCompleted, setIsCompleted] = useState(todo.isCompleted);
     return <div>
         <div className={styles.todo} key={todo.id}>
-            <input type="checkbox" checked={isCompleted} onChange={(e) => { setIsCompleted(e.target.checked) }} />
+            
+            <div className={styles.check}>
+            <input  type="checkbox" checked={isCompleted} onChange={(e) => { setIsCompleted(e.target.checked) }} />
+            </div>
+            
+            <div className={styles.task}>
             <div className={isCompleted ? styles.striked:""}>{todo.value}</div>
+            </div>
+            
+            <div className={styles.delete}>
             <button onClick={() => onDelete(todo.id)}>Delete</button>
+            </div>
+        
         </div>
     </div>
 }
